3376cec8861629ad6bafd329151be02946263b8f
Choudhary-Shahroz <chshahroz69@gmail.com>
2023-10-24 23:08:40 +0500

    First Commit

---
+++ database/db.php
@@ -0,0 +1,7 @@
+<?php
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'blog';
+    $conn = mysqli_connect($host, $username, $password, $database) or  die("Database connection failed");
+?>
